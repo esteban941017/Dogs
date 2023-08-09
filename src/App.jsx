fc972c1714d88740d49fbb6d6df026e7b382a20f
esteban941017 <esteban941017@gmail.com>
2023-08-09 20:47:01 -0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import NotFound from './Components/NotFound';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="/Dogs">
         <UserStorage>
           <Header />
           <main className="AppBody">
